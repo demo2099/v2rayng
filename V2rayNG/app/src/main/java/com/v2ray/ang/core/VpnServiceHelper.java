287700c5b62c32e61e14095f2b980548a0480c89
demo2099 <demo2099@users.noreply.github.com>
2026-09-02 07:26:42 +0800

    Use reflection for VpnService.Builder

--- V2rayNG/app/src/main/java/com/v2ray/ang/core/VpnServiceHelper.java
+++ V2rayNG/app/src/main/java/com/v2ray/ang/core/VpnServiceHelper.java
@@ -1,9 +1,12 @@
 package com.v2ray.ang.core;
 
 import android.net.VpnService;
+import java.lang.reflect.Constructor;
 
 public class VpnServiceHelper {
-    public static VpnService.Builder newBuilder() {
-        return new VpnService.Builder();
+    public static VpnService.Builder newBuilder() throws Exception {
+        Constructor<VpnService.Builder> ctor = VpnService.Builder.class.getDeclaredConstructor();
+        ctor.setAccessible(true);
+        return ctor.newInstance();
     }
 }
